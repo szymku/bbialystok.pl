6d4f840d52f90399813dceb47e4c3261c758c9c4
szymkul <szymkul@gmail.com>
2016-02-14 21:17:09 +0100

    First commit

---
+++ public_html/js/main.js
@@ -0,0 +1,7 @@
+$(function () {
+  
+   // $('tr div.styl30').eq(1).append('<span class="styl37">31.07.2014 - Medytacja nie odbędzie się. Zapraszamy w sobotę :)</span>')  
+  
+  
+}); 
+ 
---
+++ public_html/newsletter.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Buddyzm Diamentowej Drogi / Bia&#322;ystok</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="Description" content="Buddyzm Diamentowej Drogi - ośrodek w Białymstoku" />
+<meta name="Keywords" content="buddyzm, Białystok, Kagyu, buddyjski, ośrodek, medytacja,Budda, Karmapa, Dharma, nirwana, samsara, mantra, diamentowa, droga, Tybet, buddyjskie, buddyjska, buddyzmowi, buddyzmem, buddyzmie, buddyjskich, sangha, medytacje, Lama Ole Nydahl, związek buddyjski" />
+<meta name="robots" content="index, follow" />
+<link rel="shortcut icon" href="graf/favicon.ico">
+<style type="text/css">
+<!--
+body,td,th {
+	font-family: Geneva, Arial, Helvetica, sans-serif;
+	font-size: 12px;
+	color: #666666;
+}
+a {
+	color: #C1010E;
+}
+a:link {
+	text-decoration: none;
+	color: #D13131;
+}
+a:visited {
+	text-decoration: none;
+	color: #D13131;
+}
+a:hover {
+	text-decoration: underline;
+	color: #666666;
+}
+a:active {
+	text-decoration: none;
+	color: #D13131;
+}
+.styl7 {
+	font-size: 14px;
+	font-weight: bold;
+}
+.styl14 {font-size: small}
+.styl17 {color: #B00003}
+body {
+	margin-left: 0px;
+	margin-top: 5px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+}
+.styl24 {font-size: 14px}
+.styl30 {font-size: 12px}
+.styl37 {color: #D13131}
+.styl38 {font-size: 12px; color: #666666; }
+.styl42 {color: #B00003; font-weight: bold; }
+.styl43 {font-size: 13px}
+.styl45 {font-size: 11px; color: #666666; }
+-->
+</style>
+<script language="JavaScript" type="text/JavaScript">
+<!--
+function MM_reloadPage(init) {  //reloads the window if Nav4 resized
+  if (init==true) with (navigator) {if ((appName=="Netscape")&&(parseInt(appVersion)==4)) {
+    document.MM_pgW=innerWidth; document.MM_pgH=innerHeight; onresize=MM_reloadPage; }}
+  else if (innerWidth!=document.MM_pgW || innerHeight!=document.MM_pgH) location.reload();
+}
+MM_reloadPage(true);
+//-->
+</script>
+</head>
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ?
+"https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost +
+"google-analytics.com/ga.js'
+type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-6357576-1");
+pageTracker._trackPageview();
+} 
+catch(err) {}
+</script>
+
+<body>
+<div align="center"></div>
+<table width="100%" height="50"  border="0" cellpadding="0" cellspacing="0">
+  <tr>
+    <th background="graf/linia1.jpg" scope="col"></th>
+    <th scope="col"><img src="graf/logo1.jpg" width="800" height="50" /></th>
+    <th background="graf/linia1.jpg" scope="col"></th>
+  </tr>
+  <tr>
+    <th width="40%" scope="col"></th>
+    <th width="800" scope="col"><div align="left"><img src="graf/osrodek1.jpg" width="397" height="28" align="top" /></div></th>
+    <th width="40%" scope="col"></th>
+  </tr>
+</table>
+
+<table width="800" height="497" border="0" align="center" cellpadding="7" cellspacing="5">
+  <tr>
+    <td width="180" height="125" align="left" valign="top" class="styl24"><img src="graf/start1.gif" width="52" height="25" align="left" />
+      <br />    
+      <ul><span class="styl42">:. &nbsp;<a href="index.htm">g&#322;&oacute;wna</a><br />
+:. &nbsp;<a href="buddyzm.htm">buddyzm</a><br />
+:. &nbsp;<a href="wydarzenia.htm">aktualno&#347;ci</a><br />
+:. &nbsp;<a href="kontakt.htm">kontakt</a><br />
+:. &nbsp;<a href="linki.htm">linki</a></span></ul></td>
+    <td rowspan="2" valign="top" bordercolor="#666666" bgcolor="#FBF397"><span class="styl43"><br />
+    <h2 align="center">Newsletter</h2>
+    <br /><br />
+    <p>
+    	Je&#347;li chcesz otrzymywa&#263; informacje o wydarzeniach w o&#347;rodku bia&#322;ostockim oraz w o&#347;rodkach w ca&#322;ym kraju, zapisz si&#281; na nasz&#261; list&#281; mailingow&#261;. W poni&#380;szym polu nale&#380;y wpisa&#263; sw&oacute;j adres e-mail, po czym klikn&#261;&#263; na przycisk "Zapisz się".<br /><br />
+    </p>
+    <script language="php">
+	if (count($_POST))
+	{
+		////////// USTAWIENIA //////////
+		$email = 'kagyubialystok@gmail.com';	// Adres e-mail adresata
+		$subject1 = 'SUBSCRIBE';	// Temat listu
+		$subject2 = 'UNSUBSCRIBE';	// Temat listu
+		$message1 = 'Dzi&#281;kujemy za wys&#322;anie formularza rejestracyjnego.';	// Komunikat
+		$message2 = 'Dzi&#281;kujemy za wys&#322;anie formularza.';	// Komunikat
+		$message3 = '';	// Komunikat
+		$error = 'Wyst&#261;pil b&#322;&#261;d podczas wysy&#322;ania formularza.';	// Komunikat bledu
+		$charset = 'iso-8859-2';	// Strona kodowa
+		$email_wys = '';	// Adres e-mail interesanta
+		$tresc_zap = '
+Witaj,
+
+Twoj adres zostal dodany do listy e-mailowej Buddyjskiego Zwiazku
+Diamentowej Drogi Linii Karma Kagyu, Osrodka Medytacyjnego w
+Bialymstoku.
+
+Bedziesz dostawac informacje o wykladach, kursach medytacyjnych i
+imprezach buddyjskich w naszym miescie oraz w kraju.
+
+Aby wypisac sie z listy napisz e-mail z tematem UNSUBSCRIBE na adres
+bialystok@diamondway-center.org lub skorzystaj z formularza na stronie
+http://www.bialystok.buddyzm.pl/newsletter.php
+
+Zapraszamy do Osrodka!
+
+--------------------------------------------
+http://www.bialystok.buddyzm.pl/
+		
+		';	// Treść maila zwrotnego przy zapisywaniu się
+		$tresc_wyp = '
+Witaj,
+
+Twoj adres zostal usuniety z listy e-mailowej Buddyjskiego Zwiazku
+Diamentowej Drogi Linii Karma Kagyu, Osrodka Medytacyjnego w
+Bialymstoku.
+
+Od tej pory nie bedziesz dostawac informacji o wykladach, kursach medytacyjnych i
+imprezach buddyjskich.
+
+Aby zapisac sie ponownie na liste napisz e-mail z tematem SUBSCRIBE na adres
+bialystok@diamondway-center.org lub skorzystaj z formularza na stronie
+http://www.bialystok.buddyzm.pl/newsletter.php
+
+Zapraszamy do Osrodka!
+
+--------------------------------------------
+http://www.bialystok.buddyzm.pl/
+					';	// Treść maila zwrotnego przy wypisywaniu się
+		//////////////////////////////
+	
+		
+		$body = '';
+			
+			
+			
+			foreach ($_POST as $name => $value)
+			{
+				if ($name == 'mail')
+				{
+					$email_wys = $value;
+				}
+
+				if (is_array($value))
+				{
+					for ($i = 0; $i < count($value); $i++)
+					{
+						$body .= "$name: " . (get_magic_quotes_gpc() ? stripslashes($value[$i]) : $value[$i]) . "\r\n";
+					}
+				}
+				else if ($name == 'dodaj') {;}
+				else if ($name == 'usun') {;}
+				else $body .= "$name: " . (get_magic_quotes_gpc() ? stripslashes($value) : $value) . "\r\n";
+			}
+			
+			$head =
+			"MIME-Version: 1.0\r\n" .
+			"Content-Type: text/plain; charset=$charset\r\n" .
+			"Content-Transfer-Encoding: 8bit\r\n" .
+			"From: $email_wys ";
+			$head_zwrot =
+			"MIME-Version: 1.0\r\n" .
+			"Content-Type: text/plain; charset=$charset\r\n" .
+			"Content-Transfer-Encoding: 8bit\r\n" .
+			"From: bialystok@diamondway-center.org";
+			
+		</script>
+        <br /><br /><br />
+        <script language="php">
+		if ($name == 'usun')
+		{
+		echo mail($email, "=?$charset?B?" . base64_encode($subject2) . "?=", $body, $head) ? $message2 : $error;
+		echo mail($email_wys, "=?$charset?B?" . base64_encode($subject2) . "?=", $tresc_wyp, $head_zwrot) ? $message3 : $error;
+		}
+		else 
+		{
+		echo mail($email, "=?$charset?B?" . base64_encode($subject1) . "?=", $body, $head) ? $message1 : $error;
+		echo mail($email_wys, "=?$charset?B?" . base64_encode($subject1) . "?=", $tresc_zap, $head_zwrot) ? $message3 : $error;
+		}
+	}
+	else
+	{
+	</script>
+   
+	<form action="?" method="post">
+    	Imię:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="imie" size="25" /><br />
+    	Nazwisko:&nbsp;&nbsp;<input type="text" name="nazwisko" size="25" /><br />
+		E-mail:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="mail" size="25" /><br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input type="submit" name="dodaj" value="Zapisz się" />
+        <input type="submit" name="usun" value="Wypisz się" />
+	</form>
+
+	<script language="php">
+	}
+	</script>
+    <br /><br />
+    Jeśli chcesz przestać otrzymywać informacje o wydarzeniach w ośrodku wpisz swój adres e-mail w powyższym polu i kliknij na przycisk "Wypisz się" 
+    
+    </span></td>
+  </tr>
+  <tr>
+    <td valign="top">
+    <div align="right"> 
+    <a href="http://www.karmapa.pl/16karmapa/index.htm" onmouseover='karmapa.src="graf/1g.jpg"' 
+        onmouseout='karmapa.src="graf/1.jpg"' target="_blank">
+        <img src="graf/1.jpg" name="karmapa" alt="tekst alternatywny" /></a>
+        
+    	<a href="http://www.karmapa.pl/" onmouseover='trinlej.src="graf/2g.jpg"' 
+        onmouseout='trinlej.src="graf/2.jpg"' target="_blank"><img name="trinlej" src="graf/2.jpg" alt="tekst alternatywny" /></a>
+    
+    	<a href="http://www.lama-ole.pl/" onmouseover='ole.src="graf/3g.jpg"' 
+        onmouseout='ole.src="graf/3.jpg"' target="_blank"><img name="ole" src="graf/3.jpg" alt="tekst alternatywny" /></a>
+    </div> 
+        </td>
+  </tr>
+</table>
+<div align="center">
+  <hr width="80%">
+        &copy; O&#347;rodek Medytacyjny Buddyjskiego Zwi&#261;zku Diamentowej Drogi Linii Karma Kagyu w Bia&#322;ymstoku<br />
+
+
+       <!-- <script language="JavaScript">
+document.write('<a href="http://www.freestat.pl/"><img src="http://www.freestat.pl/stat-count-1a.php?id=statbud&pic=2&'+document.referrer+'" alt="FreeStat.pl - Gratis statystyki WWW" border=0></a>');
+</script>
+<noscript><a href="http://www.freestat.pl/">Freestat - statystyki gratis<img src="http://www.freestat.pl/stat-count-1a.php?id=statbud&pic=2" border=0 alt="FreeStat.pl - Gratis statystyki WWW"></a></noscript>
+-->
+
+  </div>
+</body>
+</html>
